f62c8bc410b6d0eb76f4f09967aed83ea70bea8a
michal <michalwozniak@live.ca>
2016-05-28 21:13:21 -0400

    fixed bug which was causing this https://github.com/angular/material/issues/7447

--- app/controllers/about.controller.js
+++ app/controllers/about.controller.js
@@ -13,7 +13,7 @@
         vm.user =
         {
             'headerTitle': 'About me',
-            'avatarImageUrl': 'assets/images/michalwozniak_avatar.PNG',
+            'avatarImageUrl': 'assets/images/michalwozniak_avatar.png',
             'content': 'My name is Michal, I am currently completing my Bachelor of Software Engineering at Concordia University in Montreal, Canada.<br/><br/> When I\'m not coding something, I will be having some fun with my Arduino or raspberry pie or maybe some amazing gadget found on Kickstarter. In my spare time, I enjoy going to the gym.',
             'buttonIconUrl': 'assets/icons/githubLogo.svg',
             'buttonLink': 'https://github.com/mv740/390-E-Wok',
